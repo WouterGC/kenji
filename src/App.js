a9e26f5153a05d64c977f409464272d33077e6cb
Wouter Ronteltap <wouter.ronteltap@gocustomized.com>
2018-10-21 17:56:57 +0200

    Pre test

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ class App extends Component {
     return (
         <Router>
           <div className="App">
-            <Link to="/recept">Recept</Link>
+              <Link to="/" style={{marginRight: 10, paddingLeft: 10}}>Home</Link>
+              <Link to="/recept">Recept</Link>
             <Route
-                path="/recept" render={routeProps => <Recept {...routeProps} a={'b'}/>}/>
+                path="/" render={routeProps => <Home {...routeProps} a={'b'}/>}/>
+              <Route
+                  path="/recept" render={routeProps => <Recept {...routeProps} a={'b'}/>}/>
           </div>
         </Router>
     );
@@ -26,5 +29,8 @@ const Recept = ({match, a}) => {
             <h2>Recept: {a}</h2>
         </div>)
 }
+
+const Home = (props) =>
+    <div className="homeeerrrrr"><h2>Home</h2></div>
 
 
